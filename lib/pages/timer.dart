d36d56d17316f5a15d88d0d862fa7ee02038d550
Cirrus CI <support@cirruslabs.org>
2022-12-31 10:36:54 +0000

    添加了Provider，防止离开计时器页面后崩溃，还未测试。

--- lib/pages/timer.dart
+++ lib/pages/timer.dart
@@ -1,10 +1,51 @@
 import 'dart:ui';
 import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
-// todo
+// todo 离开页面时继续计时
 // bug 运行计时器时切换页面会崩溃
-// Timer 页面
+class TimerProvider extends ChangeNotifier {
+  int _seconds = 0;
+  int _minutes = 0;
+  int _hours = 0;
+  bool _isRunning = false;
+  late Timer _timer;
+
+  int get seconds => _seconds;
+  int get minutes => _minutes;
+  int get hours => _hours;
+  bool get isRunning => _isRunning;
+
+  void startTimer() {
+    _timer = Timer.periodic(const Duration(seconds: 1), (timer) {
+      _seconds++;
+      if (_seconds == 60) {
+        _minutes++;
+        _seconds = 0;
+      }
+      if (_minutes == 60) {
+        _hours++;
+        _minutes = 0;
+      }
+      notifyListeners();
+    });
+  }
+
+  void stopTimer() {
+    _isRunning = false;
+    _timer.cancel();
+  }
+
+  void resetTimer() {
+    _seconds = 0;
+    _minutes = 0;
+    _hours = 0;
+    stopTimer();
+  }
+}
+
+// note 定时器页面
 class TimerPage extends StatefulWidget {
   const TimerPage({super.key});
 
@@ -13,35 +54,9 @@ class TimerPage extends StatefulWidget {
 }
 
 class _TimerPageState extends State<TimerPage> {
-  int _seconds = 0;
-  int _minutes = 0;
-  int _hours = 0;
-  bool _isRunning = false;
-  late Timer _timer;
-
-  void _startTimer() {
-    _timer = Timer.periodic(const Duration(seconds: 1), (timer) {
-      setState(() {
-        _seconds++;
-        if (_seconds == 60) {
-          _minutes++;
-          _seconds = 0;
-        }
-        if (_minutes == 60) {
-          _hours++;
-          _minutes = 0;
-        }
-      });
-    });
-  }
-
-  void _stopTimer() {
-    _isRunning = false;
-    _timer.cancel();
-  }
-
   @override
   Widget build(BuildContext context) {
+    final timerProvider = Provider.of<TimerProvider>(context, listen: false);
     return Scaffold(
       body: Stack(
         children: <Widget>[
@@ -50,21 +65,26 @@ class _TimerPageState extends State<TimerPage> {
               mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
                 Text.rich(
-                  TextSpan(text: '${_hours.toString().padLeft(2, '0')}:'),
+                  TextSpan(
+                      text:
+                          '${timerProvider.hours.toString().padLeft(2, '0')}:'),
                   style: const TextStyle(
                     fontSize: 80,
                     fontFeatures: [FontFeature.tabularFigures()],
                   ),
                 ),
                 Text.rich(
-                  TextSpan(text: '${_minutes.toString().padLeft(2, '0')}:'),
+                  TextSpan(
+                      text:
+                          '${timerProvider.minutes.toString().padLeft(2, '0')}:'),
                   style: const TextStyle(
                     fontSize: 80,
                     fontFeatures: [FontFeature.tabularFigures()],
                   ),
                 ),
                 Text.rich(
-                  TextSpan(text: _seconds.toString().padLeft(2, '0')),
+                  TextSpan(
+                      text: timerProvider.seconds.toString().padLeft(2, '0')),
                   style: const TextStyle(
                     fontSize: 80,
                     fontFeatures: [FontFeature.tabularFigures()],
@@ -77,36 +97,20 @@ class _TimerPageState extends State<TimerPage> {
             bottom: 10,
             right: 10,
             child: GestureDetector(
-              onTap: () {
-                setState(() {
-                  _isRunning = !_isRunning;
-                  if (_isRunning) {
-                    _startTimer();
-                  } else {
-                    _stopTimer();
-                  }
-                });
-              },
               onLongPress: () {
-                setState(() {
-                  _seconds = 0;
-                  _minutes = 0;
-                  _hours = 0;
-                  _stopTimer();
-                });
+                timerProvider.resetTimer();
               },
               child: FloatingActionButton(
                 onPressed: () {
-                  setState(() {
-                    _isRunning = !_isRunning;
-                    if (_isRunning) {
-                      _startTimer();
-                    } else {
-                      _stopTimer();
-                    }
-                  });
+                  timerProvider._isRunning = !timerProvider.isRunning;
+                  if (timerProvider.isRunning) {
+                    timerProvider.startTimer();
+                  } else {
+                    timerProvider.stopTimer();
+                  }
                 },
-                child: Icon(_isRunning ? Icons.pause : Icons.play_arrow),
+                child: Icon(
+                    timerProvider.isRunning ? Icons.pause : Icons.play_arrow),
               ),
             ),
           ),
